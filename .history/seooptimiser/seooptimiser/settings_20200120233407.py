8a91cd92db38f2399e41afb476321a3d3a321e76
Pikachu <anzhelikadarijchuk@gmail.com>
2020-01-20 23:35:19 +0200

    static

--- .history/seooptimiser/seooptimiser/settings_20200120233407.py
+++ .history/seooptimiser/seooptimiser/settings_20200120233407.py
@@ -135,5 +135,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'seooptimiser/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
