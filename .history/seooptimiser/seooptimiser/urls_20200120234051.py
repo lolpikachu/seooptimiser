2a9ed4736ec50cb5a745346f6a2e94db33b297ae
Pikachu <anzhelikadarijchuk@gmail.com>
2020-01-20 23:44:03 +0200

    static root

--- .history/seooptimiser/seooptimiser/urls_20200120234051.py
+++ .history/seooptimiser/seooptimiser/urls_20200120234051.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('', include('seooptimiser_app.urls')),
